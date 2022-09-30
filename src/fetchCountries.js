586e33aa29276fbc5355143ac62a8dc85ab6a611
Serhii Lazarenok <serhiilazar90@gmail.com>
2022-09-30 08:52:43 +0300

    fetchCountries

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,47 @@
-import axios from "axios";
+import axios from 'axios';
 
-export default { fetchCountries };
+export default class NewsfetchCountries  {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+        this.PER_PAGE = 40;
+    }
+    async fetchApiImage() {
+        axios.options = {
+            method: 'get',
+            url: 'https://pixabay.com/api/',
+            params: {
+                key: '30170611-f5f506e58232a96150936505d',
+                q: `${this.searchQuery}`,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                page: `${this.page}`,
+                per_page:`${this.PER_PAGE}`,
+            } 
+        };
+        try {
+            const response = await axios(options);
+            const data = response.data;
+            console.log(data);
+            this.incrementPage();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+       
+    }
+    incrementPage() {
+        this.page += 1;
+    }
+    resetPage() {
+        this.page = 1;
+    }
+    get query() {
+        return this.searchQuery;
+    }
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+    
+}   
--- src/index.js
+++ src/index.js
@@ -2,5 +2,76 @@ import './sass/index.scss';
 
 import SimpleLightbox from 'simplelightbox';
 import Notiflix from 'notiflix';
+import renderList from './renderList';
+import NewsfetchCountries from './fetchCountries';
 
-import fetchCountries from './fetchCountries';
+
+
+
+const refs = {
+    searchForm: document.querySelector('.js-search-form'),
+    searchFormInput: document.querySelector('.search-form__input'),
+    searchFormButton: document.querySelector('.search-form__button'),
+    fetchGallery: document.querySelector('.gallery'),
+    loadMoreButton: document.querySelector('.load-more'),
+}
+
+refs.searchFormButton.addEventListener('submit', onSearch);
+
+
+const newsFetchCountries = new NewsfetchCountries();
+console.log(newsFetchCountries);
+
+let isShown = 0;
+function onSearch(e) {
+    e.preventDefault();
+    refs.fetchGallery.innerHTML = '';
+    newsFetchCountries.query = e.currentTarget.elements.query.value.trim();
+    
+    newsFetchCountries.resetPage();
+    onRenderGallery();
+    // if (fetchCountries.query === '') {
+    //     Notify.warning('Please, fill the main field');  
+    //  return;
+    // }       
+    isShown = 1;
+    
+}
+
+function  onRenderGallery(elements) {
+    console.log(elements);
+    const markup = elements.map(({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+    }) => {
+      return `<div class="photo-card">
+      <a href="${largeImageURL}">
+        <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          ${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          ${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          ${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          ${downloads}
+        </p>
+      </div>
+      </div>`;
+    }).join('');
+         refs.fetchGallery.insertAdjacentHTML('beforeend', markup);
+  }   
